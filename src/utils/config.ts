c870ae9f05411363e64fe249062acfc3fa4472c8
shirarin <36086654+engineerinrin@users.noreply.github.com>
2018-10-11 00:34:35 +0900

    mongooseの追加と設定

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,3 +4,4 @@ dotenv.load();
 
 export const hostname = process.env.HOSTNAME as string;
 export const port = parseInt(process.env.PORT as string, 10);
+export const dbUrl = process.env.DB_URL as string;
