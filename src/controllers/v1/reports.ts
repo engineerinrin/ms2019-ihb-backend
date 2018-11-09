17dde6226b51c09d4bdd19e4d489620300c6a7bf
shirarin <36086654+engineerinrin@users.noreply.github.com>
2018-11-09 21:26:24 +0900

    add: service model

--- src/controllers/v1/reports.ts
+++ src/controllers/v1/reports.ts
@@ -1,27 +1,29 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import { Result, validationResult } from 'express-validator/check';
-import { renameSync } from 'fs';
-import path from 'path';
 import authCheck from '../../middlewares/authCheck';
 import { reportTmpUpload } from '../../middlewares/upload';
 import reportRule from '../../rules/report';
+import { createReport } from '../../services/report';
 
 const router: Router = Router();
 
 router
   // レポートの投稿
-  .post('/', [authCheck, reportTmpUpload], reportRule.post, (req: Request, res: Response, next: NextFunction) => {
+  .post('/', [reportTmpUpload, authCheck], reportRule.post, async (req: Request, res: Response, next: NextFunction) => {
     const errs: Result = validationResult(req);
 
     if (!errs.isEmpty()) {
       return res.status(400).json({ errs: errs.mapped() });
     }
 
+    const { name, title, description } = req.body;
     const { destination, filename } = req.file;
-    const tmpPath = path.join(destination, filename);
-    const staticPath = path.join('static', 'reports', filename);
 
-    renameSync(tmpPath, staticPath);
+    const { err } = await createReport(name, title, description, destination, filename);
+
+    if (err) {
+      next(err);
+    }
 
     res.status(201).send();
   });
---
+++ src/services/report.ts
@@ -0,0 +1,31 @@
+import { renameSync } from 'fs';
+import moment from 'moment';
+import path from 'path';
+import reportModel, { IReportModel } from '../models/report';
+import userModel, { IUserModel } from '../models/user';
+import { findUserByName } from './user';
+
+export const createReport = async (name: string, title: string, description: string, destination: string, filename: string) => {
+  const tmpPath = path.join(destination, filename);
+  const staticPath = path.join('static', 'reports', filename);
+
+  renameSync(tmpPath, staticPath);
+
+  try {
+    const report: IReportModel = new reportModel();
+    const { findUser } = await findUserByName(name);
+    const now = moment().format('YYYY-MM-DD HH:mm:ss');
+
+    report.title = /^[\s]*$/.test(title) ? 'なし' : title;
+    report.description = /^[\s]*$/.test(description) ? 'なし' : description;
+    report.path = filename;
+    report.author = findUser;
+    report.created_at = now;
+    report.updated_at = now;
+
+    await report.save();
+    return { err: null };
+  } catch (err) {
+    return { err };
+  }
+};
