c51fa665904b4bffdd9ca1362c411baea0ec49cd
shirarin <36086654+engineerinrin@users.noreply.github.com>
2018-11-16 17:35:42 +0900

    #31 インシデントマップ表示する

--- src/services/report.ts
+++ src/services/report.ts
@@ -8,6 +8,37 @@ import { imageAnalysisRequest } from '../utils/client';
 import incident from '../utils/incident';
 import { findUserByName } from './user';
 
+// 自分の周りのインシデントのデータを取得する
+export const getAroundMeIncidents = async (lat: number, lng: number) => {
+  try {
+    const data = await reportModel.find({
+      location: {
+        $near: {
+          $maxDistance: 1000,
+          $geometry: {
+            type: 'Point',
+            coordinates: [lng, lat],
+          },
+        },
+      },
+    }, { _id: 1, location: 1 })
+      .exec();
+
+    const incidents = [];
+    for (const i of data) {
+      incidents.push({
+        _id: i._id,
+        lng: i.location.coordinates[0],
+        lat: i.location.coordinates[1],
+      });
+    }
+
+    return { incidents };
+  } catch (err) {
+    return { err, incidents: [] };
+  }
+};
+
 export const createReport = async (name: string, title: string, description: string, destination: string, filename: string, tags: string[]) => {
   const tmpPath = path.join(destination, filename);
   const staticPath = path.join('static', 'reports', filename);
@@ -26,7 +57,7 @@ export const createReport = async (name: string, title: string, description: str
     report.path = filename;
     report.tags = tags;
     if (GPSLongitude && GPSLatitude) {
-      report.location.condinates = [GPSLongitude, GPSLatitude];
+      report.location.coordinates = [GPSLongitude, GPSLatitude];
     }
     report.author = findUser;
     report.created_at = now;
@@ -48,7 +79,7 @@ export const imageAnalysis = async (destination: string, filename: string, mimet
     const body = await imageAnalysisRequest(base64Image) as any;
     fs.unlinkSync(tmpPath);
 
-    const tags: string[] = ['テスト'];
+    const tags: string[] = [];
     for (const { description } of body.responses[0].labelAnnotations) {
       if (description in incident) {
         tags.push(incident[description]);
